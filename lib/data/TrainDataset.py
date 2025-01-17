- Fixed scale augmentation - Fixed IOU calculation

lib/data/TrainDataset.py
@@ -196,7 +196,6 @@ class TrainDataset(Dataset):
 
             mask = Image.open(mask_path).convert('L')
             render = Image.open(render_path).convert('RGB')
-            magnification = 1.0
 
             if self.is_train:
                 # Pad images
@@ -216,8 +215,6 @@ class TrainDataset(Dataset):
                 # random scale
                 if self.opt.random_scale:
                     rand_scale = random.uniform(0.9, 1.1)
-                    magnification = int(rand_scale * w) / w
-                    #print('magnification: ', magnification)
                     w = int(rand_scale * w)
                     h = int(rand_scale * h)
                     render = render.resize((w, h), Image.BILINEAR)
@@ -270,8 +267,7 @@ class TrainDataset(Dataset):
             'img': torch.stack(render_list, dim=0),
             'calib': torch.stack(calib_list, dim=0),
             'extrinsic': torch.stack(extrinsic_list, dim=0),
-            'mask': torch.stack(mask_list, dim=0),
-            'magnification': torch.tensor([float(magnification)])
+            'mask': torch.stack(mask_list, dim=0)
         }
 
 
@@ -369,7 +365,6 @@ class TrainDataset(Dataset):
         labels_v_dimless = labels_v_r / U_ref
         labels_w_dimless = labels_w_r / U_ref
         labels_p_dimless = labels_p / (rho_ref * U_ref**2)
-        #TODO: Consider magnification for characteristic time?
         timestep_dimless = timestep / (L_ref / U_ref)
 
 

lib/model/HGPIFuNet.py
@@ -422,7 +422,7 @@ class HGPIFuNet(BasePIFuNet):
         return faulty_grad
 
 
-    def get_pde_loss(self, points, magnification):
+    def get_pde_loss(self, points):
         '''
         Calculates MSE-loss of the phase advection equation and the Navier-Stokes equations (continuity and momentum equations in x,y,z)
         '''
@@ -445,9 +445,6 @@ class HGPIFuNet(BasePIFuNet):
         #print('u field mean: ', u.mean().item(), 'max: ', u.max().item(), 'min: ', u.min().item())
         #print('p field mean: ', p.mean().item(), 'max: ', p.max().item(), 'min: ', p.min().item())
 
-        # Scale characteristic length by image magnification (for physically correct curvature and surface tension calculation)
-        self.L_ref = self.L_ref / magnification
-
         # derivatives
         alpha_t = self.diff_t_de_norm(self.nth_derivative(alpha, wrt=self.t, n=1))
         alpha_x = self.diff_xyz_de_norm(self.nth_derivative(alpha, wrt=self.x, n=1))
@@ -456,7 +453,7 @@ class HGPIFuNet(BasePIFuNet):
         alpha_xx = self.diff_xyz_de_norm(self.nth_derivative(alpha_x, wrt=self.x, n=1))
         alpha_yy = self.diff_xyz_de_norm(self.nth_derivative(alpha_y, wrt=self.y, n=1))
         alpha_zz = self.diff_xyz_de_norm(self.nth_derivative(alpha_z, wrt=self.z, n=1))
-        alpha_xy = self.diff_xyz_de_norm(self.nth_derivative(alpha_x, wrt=self.z, n=1))
+        alpha_xy = self.diff_xyz_de_norm(self.nth_derivative(alpha_x, wrt=self.y, n=1))
         alpha_xz = self.diff_xyz_de_norm(self.nth_derivative(alpha_x, wrt=self.z, n=1))
         alpha_yz = self.diff_xyz_de_norm(self.nth_derivative(alpha_y, wrt=self.z, n=1))
 
@@ -505,7 +502,7 @@ class HGPIFuNet(BasePIFuNet):
         one_We = self.sigma / (self.rho_ref * self.U_ref ** 2 * self.L_ref)  # 1/We
         one_Fr2 = self.g * self.L_ref / self.U_ref ** 2  # 1/(Fr^2)
 
-        # surface tension term (requires error handling due to possible zero division)
+        # surface tension term (requires additional of small value due to possible zero division)
         abs_interface_grad = torch.sqrt(
             torch.square(alpha_x) + torch.square(alpha_y) + torch.square(alpha_z) + np.finfo(float).eps)
         curvature = - ((alpha_xx + alpha_yy + alpha_zz) / abs_interface_grad
@@ -589,7 +586,7 @@ class HGPIFuNet(BasePIFuNet):
         return conti_loss, phase_adv_loss, loss_momentum_x, loss_momentum_y, loss_momentum_z
 
     def forward(self, images, points, calibs, transforms=None, labels=None, uvwp_points=None, residual_points=None, labels_u=None,
-                labels_v=None, labels_w=None, labels_p=None, time_step=None, magnification=None, get_PINN_loss=True):
+                labels_v=None, labels_w=None, labels_p=None, time_step=None, get_PINN_loss=True):
         # Get image feature
         self.filter(images)
 
@@ -608,7 +605,7 @@ class HGPIFuNet(BasePIFuNet):
 
         # get pde errors - do not call during inference (missing gradients for model in test mode)
         if get_PINN_loss:
-            loss_conti, loss_phase_conv, loss_momentum_x, loss_momentum_y, loss_momentum_z = self.get_pde_loss(points=residual_points, magnification=magnification)
+            loss_conti, loss_phase_conv, loss_momentum_x, loss_momentum_y, loss_momentum_z = self.get_pde_loss(points=residual_points)
         else:
             loss_conti = loss_data_alpha * 0
             loss_phase_conv = loss_data_alpha * 0